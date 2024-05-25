c281d38ba0351020d2ae490d7e6b14ee24bfcdab
Francisco <franciscommvieira@hotmail.com>
2024-05-25 18:13:59 +0100

    Only main remains to be done

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -4,18 +4,45 @@
 # include <iostream>
 # include <algorithm>
 # include <vector>
-
+# include <stack>
 
 template <typename T>
-class MutantStack
+class MutantStack: public std::stack<T>
 {
-private:
-	/* data */
 public:
-	MutantStack(/* args */);
-	~MutantStack();
+	MutantStack(): std::stack<T>() {std::cout << "MutantStack Constructor called\n"; };
+	MutantStack(const MutantStack<T>& other): std::stack<T>(other) {std::cout << "MutantStack Constructor called\n";};
+	MutantStack	&operator=(const MutantStack<T>& other)
+	{
+		if (this == &other)
+			return (*this);
+		std::stack<T>::operator=(other);
+		return (*this);
+	};
+	~MutantStack() {std::cout << "MutantStack Destructor called\n"; };
+
+	typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+    typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+	//member Container: c the underlying container (protected member object)
+	
+    iterator begin() {return (this->c.begin()); };
+
+    iterator end() {return (this->c.end()); };
+
+    const_iterator begin() const {return (this->c.begin()); };
+
+    const_iterator end() const {return (this->c.end()); };
+
+    reverse_iterator rbegin() {return (this->c.rbegin()); };
+
+    reverse_iterator rend() {return (this->c.rend()); };
+
+    const_reverse_iterator rbegin() const {return (this->c.rbegin()); };
+
+    const_reverse_iterator rend() const {return (this->c.rend()); };
 };
 
-# include <iostream>
-# include <algorithm>
-# include <vector>
+#endif
